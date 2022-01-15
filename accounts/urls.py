3371d31a25aaf915a78fa98dba9377bf8f62119a
Rehmat Alam <contact@rehmat.works>
2022-01-15 19:04:50 +0500

    Registration of users

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,10 +2,12 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from .views import RegisterView
 from django.urls import path
 
 app_name='accounts'
 urlpatterns = [
+    path('register/', RegisterView.as_view(), name='user_register'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from .forms import NewUserForm
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+
+class RegisterView(APIView):
+    http_method_names = ['post']
+    permission_classes = []
+    
+    def post(self, request, *args, **kwargs):
+        form = NewUserForm(data=request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            tok = RefreshToken.for_user(user)
+            return Response({
+                'status': True,
+                'tokens': {
+                    'refresh': str(tok),
+                    'access': str(tok.access_token)
+                }
+            }, status=status.HTTP_200_OK)
+        
+        return Response({
+            'status': False,
+            'errors': form.errors
+        }, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
